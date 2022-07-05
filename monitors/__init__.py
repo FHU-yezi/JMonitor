8752b40623e1b8eba396cbf8d3f3fc9be77924fa
FHU-yezi <yehaowei20060411@qq.com>
2022-07-05 23:22:02 +0800

    feat: 增加监控函数

--- monitors/__init__.py
+++ monitors/__init__.py
@@ -1,2 +1,7 @@
+from monitors import beikeisland, jianshu, ssservices
+
+
 def init_monitors():
-    pass  # 为了运行该模块，继而对监控任务进行注册操作
+    beikeisland
+    jianshu
+    ssservices
---
+++ monitors/jianshu.py
@@ -0,0 +1,12 @@
+from helper import WebGetRequestHelper
+from register import MonitorFunc
+
+
+@MonitorFunc("jianshu", "website", "0 1-59 * * * *")
+def jianshu_website():
+    return WebGetRequestHelper("https://www.jianshu.com")
+
+
+@MonitorFunc("jianshu", "api", "0 1-59 * * * *")
+def jianshu_api():
+    return WebGetRequestHelper("https://www.jianshu.com/asimov/users/slug/ea36c8d8aa30")
