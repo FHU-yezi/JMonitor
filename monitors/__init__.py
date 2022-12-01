fc6805452909e6d037bd1da74f2a75aecc157f5d
yezi <yehaowei20060411@qq.com>
2022-12-01 10:38:01 +0800

    feat: 删除已停运和下线的服务监控任务

--- monitors/__init__.py
+++ monitors/__init__.py
@@ -4,6 +4,5 @@ from monitors import beikeisland, jianshu, ssservices
 def init_monitors():
     """初始化监控函数
     """
-    beikeisland
     jianshu
     ssservices
--- monitors/ssservices.py
+++ monitors/ssservices.py
@@ -5,8 +5,3 @@ from register import monitor
 @monitor("jianshu_micro_features", "website", "0 0/5 * * * *")
 def jianshu_micro_features_website():
     return HTTP_get_request_helper("http://120.27.239.120:8602/")
-
-
-@monitor("wind_with_2021", "website", "0 0/5 * * * *")
-def wind_with_2021_website():
-    return HTTP_get_request_helper("http://120.27.239.120:8603")
